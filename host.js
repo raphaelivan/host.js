92ecaa1a75539a01a8b58a427165e26d01e3f01e
Raphael Ivan <raphaivan@gmail.com>
2014-01-10 14:19:28 -0200

    Add some methods to  object

--- host.js
+++ host.js
@@ -1,8 +1,21 @@
 ;(function(window)  {
   "use strict";
 
-  var host = {}
+  var
+    host = {}
   , location = window.location || document.location;
+
+  host.location = function (loca) {
+    return loca ? location.assign(loca) : location.href;
+  }
+
+  host.protocol = function (protocol) {
+    return protocol ? (location.protocol = protocol) : location.protocol;
+  }
+
+  host.reloadPage = function () {
+    location.reload(true);
+  }
 
   //
   // Exposes the $host object to global object
