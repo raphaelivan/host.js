edaa6878905577039b67c278d991f1a4cbe99844
Raphael Ivan <raphaivan@gmail.com>
2014-01-10 14:34:49 -0200

    creates search methods

--- host.js
+++ host.js
@@ -21,6 +21,35 @@
     location.hash += "#" + hash;
   }
 
+  host.search = function  (search) {
+    if (search) {
+      return location.search = transformHashOnString(search);
+    };
+
+    return transformStringOnHash(location.search);
+  }
+
+  var transformStringOnHash = function (search) {
+    var searchObject  = {};
+
+    if (search.length > 1) {
+      for (var nkey = 0,  keys = search.substr(1).split("&"); nkey < keys.length; nkey++) {
+        var aKeys = keys[nkey].split("=");
+          searchObject[aKeys[0]] = aKeys[1];
+      };
+    };
+    return searchObject;
+  }
+
+  var transformHashOnString = function (search) {
+    var searchString = '';
+
+    for (var k in search) {
+      searchString +=  (searchString.length > 1 ? '&' : '') + k + "=" + search[k];
+    }
+    return searchString;
+  }
+
   //
   // Exposes the $host object to global object
   return window.$host = host;
